054d4fbb4a14e6b9afda08e72e17570cb32c6886
Sreenath <info@uniquebees.com>
2023-11-16 11:56:42 +0530

    Push

--- src/pages/businessEdit.js
+++ src/pages/businessEdit.js
@@ -12,22 +12,20 @@ function BusinessEdit() {
   return (
     <VStack width="90%" mx="3" maxW="300px">
       <FormControl isRequired>
-        <FormControl.Label  _text={{ bold: true }}> 
-          Name
-        </FormControl.Label>
+        <FormControl.Label  _text={{ bold: true }}>  Name </FormControl.Label>
         <Input  placeholder="John" onChangeText={value => setData({ ...formData, name: value })} />
-        <FormControl.HelperText  _text={{  fontSize: "xs" }}>
-         {formData.name.length<3 ?" Name should contain atleast 3 character.":""}
-        </FormControl.HelperText>
-        <FormControl.ErrorMessage _text={{ fontSize: "xs" }} >
-          Error Name
-        </FormControl.ErrorMessage>
+        <FormControl.HelperText  _text={{  fontSize: "xs" }}> {formData.name.length<3 ?" Name should contain atleast 3 character.":""} </FormControl.HelperText>
+        <FormControl.ErrorMessage _text={{ fontSize: "xs" }} > Error Name </FormControl.ErrorMessage>
+      </FormControl>
+      <FormControl isRequired>
+        <FormControl.Label  _text={{ bold: true }}>  Address </FormControl.Label>
+        <Input  placeholder="Address" onChangeText={value => setData({ ...formData, name: value })} />
+        <FormControl.HelperText  _text={{  fontSize: "xs" }}> {" Address"} </FormControl.HelperText>
+        <FormControl.ErrorMessage _text={{ fontSize: "xs" }} > Error Name </FormControl.ErrorMessage>
       </FormControl>
     </VStack>
   )
-}
-
- 
+} 
 export default () => {
   return (
     <NativeBaseProvider>
