7438a60c2d378a566be46dbc16db4442dcfe670b
Sreenath <info@uniquebees.com>
2023-11-22 15:15:32 +0530

    Axios integration

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,13 +6,9 @@ import {Text, View,StatusBar,Alert} from 'react-native';
 import {sentNotification} from '../notification/appNotification'
 
 function Home (){ 
- 
-   
     return (
-        
-       
-    <VStack   bg="amber.400" >
-        <Center ml={25} w="80%" h="80%" bg="indigo.300" rounded="md" shadow={3} >
+    <VStack   bg="amber.400" h="100%" >
+        <Center ml={25} mt={25} mr={25} mb={50} h="80%" bg="indigo.300" rounded="md" shadow={3} >
     <Text>Home1</Text>  
     <TouchableOpacity onPress={()=>sentNotification("Home")}>
         <Text>Sent Notification1</Text>
