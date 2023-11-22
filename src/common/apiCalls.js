68478982c89dcd460e10cbe20ccb9e7e3b3c7c81
Sreenath <info@uniquebees.com>
2023-11-22 18:50:03 +0530

    Api calls call format

--- src/common/apiCalls.js
+++ src/common/apiCalls.js
@@ -1,29 +1,31 @@
 import React from "react";
-import axios from "axios"; 
-const baseUrl='https://api.taswiq.app';
+import axios from "axios";
+const baseUrl = 'https://api.taswiq.app';
 
-export const  getToken=(domain)=>{ 
-    const formData=new FormData();
-    domain='credozone'
-    formData.append('domain',domain);
-   // formData.append('username','fasil');
-    //formData.append('password','abcd1234');
+export const getToken = (_domain, _username, _password) => {
+    const formData = new FormData(); 
+    formData.append('domain', _domain);
+    formData.append('username', _username);
+    formData.append('password', _password);
     axios({
-        method:"POST",
-        formData:formData,
-        url:`${baseUrl}/company`
-    }).then(res=>console.log(res)).catch(error=>console.log(error))
-   
-    
+        method: "POST",
+        formData: formData,
+        url: `${baseUrl}/token`
+    }).then(res => {
+        console.log(res.data)
+        return res.data;
+    })
+    .catch(error => {return error;})
+
 }
+
 
 //Test Call
-const baseUrl1="https://jsonplaceholder.typicode.com"
-export const getApi=()=>{
+const baseUrl1 = "https://jsonplaceholder.typicode.com"
+export const getApi = () => {
     axios({
-        method:"GET",
-        url:`${baseUrl1}/posts`
-    }).then(res=>console.log(res)).catch(error=>console.log(error))
+        method: "GET",
+        url: `${baseUrl1}/posts`
+    }).then(res => console.log(res)).catch(error => console.log(error))
 }
 
- 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,28 @@
 
-import { Button ,VStack,Center } from 'native-base';
-import React from 'react';  
-import { TouchableOpacity ,PermissionsAndroid } from 'react-native';
-import {Text, View,StatusBar,Alert} from 'react-native';
-import {sentNotification} from '../notification/appNotification'
-import {getToken,getApi} from '../common/apiCalls'
-import axios from "axios"; 
+import { Button, VStack, Center } from 'native-base';
+import React from 'react';
+import { TouchableOpacity, PermissionsAndroid } from 'react-native';
+import { Text, View, StatusBar, Alert } from 'react-native';
+import { sentNotification } from '../notification/appNotification'
+import { getToken, getApi } from '../common/apiCalls'
+import axios from "axios";
 
-function Home (){ 
+function Home() {
 
     return (
-    <VStack   bg="amber.400" h="100%" >
-        <Center ml={25} mt={25} mr={25} mb={50} h="80%" bg="indigo.300" rounded="md" shadow={3} >
-    <Text>Home1</Text>  
-    <TouchableOpacity onPress={()=>sentNotification("Home")}>
-        <Text>Sent Notification</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>getToken()}>
-<Text>Get Token12</Text>
-        </TouchableOpacity>
-        </Center>
-     </VStack >
-     
-     
+        <VStack bg="amber.400" h="100%" >
+            <Center ml={25} mt={25} mr={25} mb={50} h="80%" bg="indigo.300" rounded="md" shadow={3} >
+                <Text>Home1</Text>
+                <TouchableOpacity onPress={() => sentNotification("Home")}>
+                    <Text>Sent Notification</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => getToken("credozone","fasil","abcd1234")}>
+                    <Text>Get Token12</Text>
+                </TouchableOpacity>
+            </Center>
+        </VStack >
+
+
     )
 }
 export default Home;
